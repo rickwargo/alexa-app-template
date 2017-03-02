9e231e886adc9ce2bb199e080a0586d74ab85781
rickwargo <github@epicminds.com>
2017-03-02 17:54:42 -0500

    change display of basic appId

--- config/app-config.js
+++ config/app-config.js
@@ -2,7 +2,7 @@
 'use strict';
 
 var appConfig = {
-    applicationId: 'amzn1.ask.skill.00000000-0000-0000-0000-000000000000',   // Must update or all calls will fail on appIntent.pre()
+    applicationId: 'amzn1.ask.skill.00000000-0000-0000-0000-000000000000', // Don't update here, update applicationid.json
     applicationName: 'alexa-app-template',           // Must update this - no spaces, should be a valid identifier (hypens ok)
     functionName: 'alexa-app-template-skill',        // Must update or gulp test-lambda will fail
     description: 'A starter template to build your Alexa skill'
